d51dea0eda50af6914f60f0c19e3049c1e6c34c9
Tim <tim.policar@gmail.com>
2019-11-28 18:02:08 +0100

    intent and content

--- app/src/main/java/com/timpolicar/swoosh/MainActivity.kt
+++ app/src/main/java/com/timpolicar/swoosh/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.timpolicar.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getStartedBtn.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java) //to je eksplicitni intent.(eksplicitni: v appih, ker veš točno kateri class hočeš nardit
+            // implicitni: neves tocno kater class nardit npr. pr browserjih (google, firefox), neves kater je na mobitelu kater ne
+            // ta tu je eksplicitni)
+            //context: v tem primeru podamo kontekst trenutnega activitija, podamo vse trenutne informacije
+            startActivity(leagueIntent) //pozeni ta activity, prej smo samo kreirali
+        }
     }
 }
