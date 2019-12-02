f29a2decc684f3f60bc49e425d1d2c177ca8a176
Tim <tim.policar@gmail.com>
2019-12-02 10:39:35 +0100

    lifecycle example, thirs layout

--- app/src/main/java/com/timpolicar/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/timpolicar/swoosh/LeagueActivity.kt
@@ -1,12 +1,20 @@
 package com.timpolicar.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
+    }
+
+    fun leagueNextClicked(view: View){
+        val skillActivity = Intent(this, SkillActivity::class.java)
+        startActivity(skillActivity)
     }
 }
--- app/src/main/java/com/timpolicar/swoosh/MainActivity.kt
+++ app/src/main/java/com/timpolicar/swoosh/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) { //onCreate is called only once in its lifetime!!
         super.onCreate(savedInstanceState)
